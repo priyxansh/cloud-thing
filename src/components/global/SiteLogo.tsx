44f22737ef9f4fa52019308bf4e069cd9fda5f18
Priyansh Rai <priyanshrai16@gmail.com>
2025-03-10 14:15:52 +0530

    Update SiteLogo component to display logo text with updated styling and add SITE_LOGO_TEXT constant

--- src/components/global/SiteLogo.tsx
+++ src/components/global/SiteLogo.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
-import { SITE_TITLE } from "~/constants/site-config";
+import { SITE_LOGO_TEXT } from "~/constants/site-config";
 
 const SiteLogo = () => {
+  const siteLogoText = SITE_LOGO_TEXT.split(" ");
+  const firstWord = siteLogoText[0];
+  const lastWord = siteLogoText.at(-1);
+
   return (
     <Link href={`/`} className="relative flex items-center justify-center">
-      <h1 className="text-lg font-bold text-primary sm:text-xl">
-        {SITE_TITLE}
+      <h1 className="text-lg font-semibold text-primary sm:text-xl sm:font-bold">
+        <span className="text-foreground">{firstWord}</span>
+        <span>{lastWord}</span>
       </h1>
     </Link>
   );
--- src/constants/site-config.ts
+++ src/constants/site-config.ts
@@ -1,2 +1,3 @@
 // For global site related constants
 export const SITE_TITLE = "CloudThing";
+export const SITE_LOGO_TEXT = "Cloud Thing";
